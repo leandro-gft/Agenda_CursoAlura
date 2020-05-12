fabaa0575fdbd3328750cce276c353bb6013fbd2
leandro-gft <lpsacchi@gmail.com>
2020-05-12 18:11:18 -0300

    Implementando uma ListView

--- app/src/main/java/br/com/gft/agenda/MainActivity.java
+++ app/src/main/java/br/com/gft/agenda/MainActivity.java
@@ -3,6 +3,16 @@ package br.com.gft.agenda;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +20,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView listaAlunos = findViewById(R.id.lvAlunos);
+
+        List<String> alunos = new ArrayList<>(Arrays.asList("Alex", "Fran", "Jose", "Leandro", "Ana"));
+
+        listaAlunos.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, alunos));
+
+
+
     }
 }
